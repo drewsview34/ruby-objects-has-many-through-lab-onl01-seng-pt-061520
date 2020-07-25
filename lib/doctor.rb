760f68622f18aac52cc045149fd209a7d9280fa9
Andrew Hinojosa <drewsview34@outlook.com>
2020-07-25 02:49:41 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,6 +22,6 @@ class Doctor
   end  
         
   def patients 
-    appointments.map { |appointment| appointment.patient }
+    appointments.map { |appointment| appointment.patient  }
   end
 end
